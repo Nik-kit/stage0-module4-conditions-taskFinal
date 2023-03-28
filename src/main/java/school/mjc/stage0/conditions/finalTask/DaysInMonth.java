6a26ba757f002d0fe3228e49feddf3285ceb82d6
Nik-kit <nikon12344@mail.com>
2023-03-29 01:55:14 +0400

    n

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,43 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        boolean leap;
+        if(year % 100 == 0) {
+            if(year % 400 == 0) {
+                leap = true;
+            } else {
+                leap = false;
+            }
+        } else {
+            if (year % 4 == 0) {
+                leap = true;
+            } else {
+                leap = false;
+            }
+        }
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                System.out.println(31);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                System.out.println(30);
+                break;
+            case (2):
+                if(leap) {System.out.println(29);}
+                else {System.out.println(28);}
+                break;
+            default:
+                System.out.println("invalid date");
+                break;
+        }
     }
 }
